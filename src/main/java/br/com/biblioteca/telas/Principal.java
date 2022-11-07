b717154e5c155e19566057a28552640856033db1
Lucas Nunes <lucas.nunes@senior.com.br>
2022-11-07 14:43:44 -0300

    Ajustando tela

--- src/main/java/br/com/biblioteca/telas/Principal.java
+++ src/main/java/br/com/biblioteca/telas/Principal.java
@@ -3,6 +3,7 @@ package main.java.br.com.biblioteca.telas;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,14 +13,15 @@ import javax.swing.border.EmptyBorder;
 public class Principal extends JFrame {
 
 	private static final long serialVersionUID = 3758927955378564092L;
+
 	private JPanel contentPanel;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
+		EventQueue.invokeLater(
+			() -> {
 				try {
 					Principal frame = new Principal();
 					frame.setVisible(true);
@@ -27,7 +29,7 @@ public class Principal extends JFrame {
 					e.printStackTrace();
 				}
 			}
-		});
+		);
 	}
 
 	/**
@@ -35,7 +37,7 @@ public class Principal extends JFrame {
 	 */
 	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 642, 406);
+		setBounds(100, 100, 1132, 746);
 		contentPanel = new JPanel();
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -44,41 +46,48 @@ public class Principal extends JFrame {
 
 		JPanel painelBarraLateral = new JPanel();
 		painelBarraLateral.setBackground(new Color(255, 0, 128));
-		painelBarraLateral.setBounds(0, 0, 53, 367);
+		painelBarraLateral.setBounds(0, 0, 91, 707);
 		contentPanel.add(painelBarraLateral);
 
 		JPanel painelPrincipal = new JPanel();
-		painelPrincipal.setBounds(55, 0, 571, 367);
+		painelPrincipal.setBounds(92, 0, 1024, 707);
 		contentPanel.add(painelPrincipal);
 		painelPrincipal.setLayout(new CardLayout(0, 0));
 
-		JPanel livroPainel = new JPanel();
-		livroPainel.setBackground(Color.RED);
-		painelPrincipal.add(livroPainel, "livroPainelCard");
-		livroPainel.setLayout(null);
+		JPanel painelLivro = new JPanel();
+		painelLivro.setBackground(Color.RED);
+		painelPrincipal.add(painelLivro, "livroPainelCard");
+		painelLivro.setLayout(null);
 
-		JPanel autorPainel = new JPanel();
-		autorPainel.setBackground(Color.CYAN);
-		painelPrincipal.add(autorPainel, "autorPainelCard");
-		autorPainel.setLayout(null);
+		JPanel painelAutor = new JPanel();
+		painelAutor.setBackground(Color.CYAN);
+		painelPrincipal.add(painelAutor, "autorPainelCard");
+		painelAutor.setLayout(null);
 
 		JButton livroMenuBotao = new JButton("Livro");
 		livroMenuBotao.addActionListener(
-			actionListener -> {
-				CardLayout cardLayout = (CardLayout) painelPrincipal.getLayout();
-				cardLayout.show(painelPrincipal, "livroPainelCard");
-			}
+			adicionarCardNoPainel(
+				painelPrincipal,
+				"livroPainelCard"
+			)
 		);
 
 		JButton autorMenuBotao = new JButton("Autor");
 		autorMenuBotao.addActionListener(
-			actionListener -> {
-				CardLayout cardLayout = (CardLayout) painelPrincipal.getLayout();
-				cardLayout.show(painelPrincipal, "autorPainelCard");
-			}
+			adicionarCardNoPainel(
+				painelPrincipal,
+				"autorPainelCard"
+			)
 		);
 
 		painelBarraLateral.add(autorMenuBotao);
 		painelBarraLateral.add(livroMenuBotao);
+	}
+	
+	private ActionListener adicionarCardNoPainel(JPanel painel, String nomeDoFilho) {
+		return actionListener -> {
+			CardLayout cardLayout = (CardLayout) painel.getLayout();
+			cardLayout.show(painel, nomeDoFilho);
+		};
 	}
 }
